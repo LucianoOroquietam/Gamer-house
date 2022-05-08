e5c412a4c756d2859f02e406d2d736ef19d623be
Tomás Tourn <tomytourn1@gmail.com>
2022-05-08 02:36:53 -0300

    agrego captcha

---
+++ js/captcha.js
@@ -0,0 +1,44 @@
+"use strict";
+//llamo al boton
+document.querySelector("#btnEnviar").addEventListener("click",comprobrarCaptcha);
+
+let random= Math.floor(Math.random()*5)
+MostrarImg();
+
+function MostrarImg(){
+   switch(random){
+       case 0:{
+        document.querySelector("#imgCaptcha").src="img/captcha/resultado0.jpg"
+        break;
+       }
+       case 1:{
+        document.querySelector("#imgCaptcha").src="img/captcha/resultado1.jpg"
+        break;
+       }
+       case 2:{
+        document.querySelector("#imgCaptcha").src="img/captcha/resultado2.jpg"
+        break;
+       }
+       case 3:{
+        document.querySelector("#imgCaptcha").src="img/captcha/resultado3.jpg"
+        break;
+       }
+       case 4:{
+        document.querySelector("#imgCaptcha").src="img/captcha/resultado4.jpg"
+        break;
+       }
+      
+   }
+}
+
+function comprobrarCaptcha(){
+    event.preventDefault();
+    let numIngresado= document.querySelector("#respuestaIngresada").value;
+    if(numIngresado==random){
+        document.querySelector("#resultado").innerHTML="Correcto, Registro valido";
+        console.log("hola")
+    }
+    else{
+        document.querySelector("#resultado").innerHTML="Incorrecto, vuelva a intentarlo"
+    }
+}
