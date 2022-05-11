23276dd92b7f2f229154e3084f1e8856235477fe
Luciano Oroquieta Merlino <oroquietaluciano@gmail.com>
2022-05-11 15:08:43 -0300

    agrego estilos al captcha

--- js/captcha.js
+++ js/captcha.js
@@ -1,10 +1,15 @@
 "use strict";
+
 //llamo al boton
 document.querySelector("#btnEnviar").addEventListener("click",comprobrarCaptcha);
 
-let random= Math.floor(Math.random()*5)
-MostrarImg();
+//variable contenedora de un random
+let random= Math.floor(Math.random()*5) 
 
+
+
+
+//funcion que me genera el random
 function MostrarImg(){
    switch(random){
        case 0:{
@@ -31,17 +36,26 @@ function MostrarImg(){
    }
 }
 
-function comprobrarCaptcha(){
+//llamo a la funcion para q se muestre un imagen por pantalla
+MostrarImg();
+
+
+//verifico lo que ingresa el usuario
+function comprobrarCaptcha(event){
     event.preventDefault();
     let numIngresado= document.querySelector("#respuestaIngresada").value;
     if(numIngresado==random){
         document.querySelector("#resultado").innerHTML="Correcto, Registro valido";
         document.querySelector("#estilocaptcha").classList.add("verificado");
         document.querySelector("#estilocaptcha").classList.remove("error");
+        document.querySelector("#verificado-incorrecto").classList.add("mostrarimagen")
+        document.querySelector("#verificado-incorrecto").src="img/captcha/verficiado.svg"
     }
     else{
         document.querySelector("#resultado").innerHTML="Incorrecto, vuelva a intentarlo"
         document.querySelector("#estilocaptcha").classList.add("error");
         document.querySelector("#estilocaptcha").classList.remove("verificado");
+        document.querySelector("#verificado-incorrecto").classList.add("mostrarimagen")
+        document.querySelector("#verificado-incorrecto").src="img/captcha/error.svg"
     }
 }
